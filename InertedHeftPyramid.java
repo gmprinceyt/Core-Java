120f77753c9caab0674f06df7b0a3f28fb579e69
PRINCE KUAMR <gmprince420@gmail.com>
2025-08-10 21:00:15 +0530

    'InertedHeftPyramidWithNumber'

--- InertedHeftPyramid.java
+++ InertedHeftPyramid.java
@@ -2,22 +2,43 @@
 public class InertedHeftPyramid {
 
     public static void InertedAndRotatedHeftPyramid(int row) {
+
+        //           * 
+        //        *  * 
+        //     *  *  * 
+        //  *  *  *  * 
         // rows
         for (int i = 1; i <= row; i++) {
-            //cols
+        //cols
             //spaces
             for (int k = row - i; k >= 1; k--) {
-                System.out.print("_");
+                System.out.print("   ");
             }
             for (int j = 1; j <= i; j++) {
-                //print 
-                System.out.print("*");
+            //print 
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    };
+
+    public static void InertedHeftPyramidWithNumber(int row){
+        // 12345
+        // 1234
+        // 123
+        // 12
+        // 1
+        for (int i = row; i >= 1;  i--){
+            //inner loop 
+            for (int j = 1; j <= i; j++){
+                System.out.print(j);
             }
             System.out.println();
         }
     }
 
     public static void main(String args[]) {
-        InertedAndRotatedHeftPyramid(4);
+        // InertedAndRotatedHeftPyramid(4);
+        InertedHeftPyramidWithNumber(5);
     };   
 }
