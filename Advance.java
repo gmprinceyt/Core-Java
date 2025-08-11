81085dfc29b5ca2e0938da5f6145b93b204f9da5
PRINCE KUAMR <gmprince420@gmail.com>
2025-08-11 23:21:54 +0530

    butterfly pattern

---
+++ Advance.java
@@ -0,0 +1,54 @@
+public class Advance {
+
+        // *        *
+        // **      **
+        // ***    ***
+        // ****  ****
+        // **********
+        // **********
+        // ****  ****
+        // ***    ***
+        // **      **
+        // *        *
+    public static void ButterFly(int n ){
+        for (int row = 1; row <= n; row++ ){
+
+            //start
+            for (int j = 1; j <= row; j++){ 
+                System.out.print("*");
+            };
+
+            //space
+            for (int j = 1; j <= 2*(n-row); j++ ){
+                System.out.print(" ");
+            };
+
+            //start
+            for (int j = 1; j <= row; j++){ 
+                System.out.print("*");
+            };
+            System.out.println();
+        }
+        for (int row = n; row >= 1; row-- ){
+           //start
+            for (int j = 1; j <= row; j++){ 
+                System.out.print("*");
+            };
+
+            //space
+            for (int j = 1; j <= 2*(n-row); j++ ){
+                System.out.print(" ");
+            };
+
+            //start
+            for (int j = 1; j <= row; j++){ 
+                System.out.print("*");
+            };
+            System.out.println();
+        }
+    } 
+
+    public static void main(String ags []){
+        ButterFly(3);
+    }
+}
