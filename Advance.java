93830d8d20e49c0c180711634ddededd67dba6fa
PRINCE KUAMR <gmprince420@gmail.com>
2025-08-13 21:41:36 +0530

    hollow solid rhomnus

--- Advance.java
+++ Advance.java
@@ -59,11 +59,31 @@ public class Advance {
             };
             System.out.println();
         }
+    };
+
+    public static void HollowSolidRhombus(int n ){
+        for (int row = 1; row <= n; row++){
+            //space 
+            for (int j = 1; j <= n-row; j++){
+                System.out.print(" ");
+            }
+            //star
+            for (int j = 1; j <= n; j++){
+                if (row == 1 || row == n || j == 1|| j== n){
+                System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            };
+            System.out.println();
+        }
     }
 
 
     public static void main(String ags []){
         // ButterFly(3);
-        SolidRhombus(5);
+        // SolidRhombus(5);
+        HollowSolidRhombus(5);
+        
     }
 }
