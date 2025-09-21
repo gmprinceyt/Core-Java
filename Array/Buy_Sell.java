fd0e8a050945865b8d86819b6c93d4431c9f376a
PRINCE KUAMR <gmprince420@gmail.com>
2025-09-21 22:03:00 +0530

    Buy and Sell

--- Array/Buy_Sell.java
+++ Array/Buy_Sell.java
@@ -1,5 +1,7 @@
 class Buy_Sell {
  
+
+    // Selft Logic Code 
     public static int  BuyAndSell(int arr[]){
         int minBuyingPrice = arr[0]; 
         int profit = 0; 
@@ -12,13 +14,30 @@ class Buy_Sell {
             }
             if (minBuyingPrice > arr[i]) minBuyingPrice= arr[i];
         };
-
         return profit;
+    };
+
+    //  Other Code 
+    public static int BuyAndSell2(int [] prices){
+        int maxProfit = 0;
+        int minBuyingPrice = prices[0];
+
+        for (int p = 0; p < prices.length; p++ ){
+            if (minBuyingPrice < prices[p]){
+                int todayProfit = prices[p]-minBuyingPrice;
+                maxProfit = Math.max(todayProfit, maxProfit);
+            } else {
+                minBuyingPrice = prices[p];
+            }
+        }
+        return maxProfit; 
+         
     }
+
 
     public static void main(String args []){
         int arr[] = {80,3, 2,1,3,4,40};
-        int profit = BuyAndSell(arr);
+        int profit = BuyAndSell2(arr);
         System.out.print(profit);
     }
 }
