7b6f1ee8e77e7b8a6e8ed324ff23dbf582f9d010
PRINCE KUAMR <gmprince420@gmail.com>
2025-10-25 22:54:21 +0530

    bubble Sort Practice

---
+++ Sorting/Bubble.java
@@ -0,0 +1,20 @@
+class Bubble {
+    public static void BubbleSort(int arr[]){
+        for (int i = 0; i < arr.length; i++){
+            for (int j = i+1; j < arr.length; j++){
+                if  (arr[i] > arr[j]){
+                    int t = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = t;
+                };
+            };
+        };
+    }
+    public static void main(String  args [] ){
+        int arr[] = {5,4,1,3,2};
+        BubbleSort(arr);
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
